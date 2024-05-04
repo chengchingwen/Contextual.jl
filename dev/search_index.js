e48a072e68a13978f6cae391cb1b8ed098bb1a76
Documenter.jl <documenter@juliadocs.github.io>
2024-05-04 12:47:21 +0000

    build based on 7ba4652

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Contextual","category":"page"},{"location":"#Contextual","page":"Home","title":"Contextual","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Contextual.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Contextual]","category":"page"}]
+}
